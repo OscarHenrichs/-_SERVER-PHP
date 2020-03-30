d665f9da441c90d1f2f77f1d4d09bb821f96d9d3
Oscar Henrichs PessoaL <oscar.henrichs.work@outlook.com>
2020-03-30 18:56:54 -0300

    Add commit

--- index.php
+++ index.php
@@ -14,6 +14,21 @@
             <ul class="list-group">
             <?php foreach($server as $key => $value): ?>
                 <li class="list-group-item">
+                    <strong><?php echo $key; ?> : </strong>
+                    <?php echo $value; ?>
+                </li>
+            <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+
+        <h1>Client & File Info</h1>
+        <?php if ($client) : ?>
+            <ul class="list-group">
+            <?php foreach($client as $key => $value): ?>
+                <li class="list-group-item">
+                    <strong><?php echo $key; ?> : </strong>
+                    <?php echo $value; ?>
+                </li>
             <?php endforeach; ?>
             </ul>
         <?php endif; ?>
